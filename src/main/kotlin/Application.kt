05c840b67066c4761ab15e992947be12386fbc9e
Selva <k.n.selvakumar@gmail.com>
2016-11-28 09:14:03 +0530

    preparing for heroku

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,34 +1,36 @@
 import TodoReqHandler.handler
 import org.rapidoid.http.Req
-import org.rapidoid.setup.On
+import org.rapidoid.setup.Setup
 
 fun main(args: Array<String>) {
     val store = TodoStore.get()
 
     val getTodo: (Req, String) -> Todo = { req, uuid -> store.get("${baseUrl(req)}/$uuid") }
 
-    On.get("/").json(handler { store.all() })
+    val app = Setup.create("app").port(Integer.parseInt(System.getProperty("server.port", "8888")))
 
-    On.post("/").json(handler { r ->
+    app.get("/").json(handler { store.all() })
+
+    app.post("/").json(handler { r ->
         store.save(Todo.build(r.data(), baseUrl(r)))
     })
 
-    On.get("/{uuid}").json(handler { r ->
+    app.get("/{uuid}").json(handler { r ->
         getTodo(r, r.data("uuid"))
     })
 
-    On.delete("/{uuid}").json(handler { r ->
+    app.delete("/{uuid}").json(handler { r ->
         store.delete(getTodo(r, r.data("uuid")))
     })
 
-    On.patch("/{uuid}").json(handler { r ->
+    app.patch("/{uuid}").json(handler { r ->
         val todo = getTodo(r, r.data("uuid"))
         store.save(todo.update(r.data()))
     })
 
-    On.delete("/").json(handler { store.deleteAll() })
+    app.delete("/").json(handler { store.deleteAll() })
 
-    On.options("/*").json(handler { })
+    app.options("/*").json(handler { })
 
 }
 
